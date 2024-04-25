f82d3b11810e2096a184651fa3a74fef701724eb
Nimila Samarasinghe <nimilas@hsenidmobile.com>
2024-04-25 14:19:46 +0530

    New: JmDNS added

--- app/src/main/java/io/applova/mongodbatlasdemo/MainActivity.java
+++ app/src/main/java/io/applova/mongodbatlasdemo/MainActivity.java
@@ -1,10 +1,16 @@
 package io.applova.mongodbatlasdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,10 +18,19 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
 
 import io.applova.mongodbatlasdemo.adapter.PersonAdapter;
-import io.applova.mongodbatlasdemo.async.TaskCallback;
 import io.applova.mongodbatlasdemo.databinding.ActivityMainBinding;
 import io.applova.mongodbatlasdemo.domain.Person;
 import io.realm.Realm;
@@ -38,6 +53,10 @@ public class MainActivity extends AppCompatActivity {
     private Realm realm;
     private PersonAdapter adapter;
     private Context context;
+    private JmDNS jmdns;
+    private ConnectivityManager connectivityManager;
+    private boolean isOnline = true;
+    private final String APP_NAME = "io.applova.mongodbatlasdemo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,13 +88,7 @@ public class MainActivity extends AppCompatActivity {
                         realm = realmInstance;
                         adapter = new PersonAdapter(new ArrayList<>(), realm, result -> {
                             if (result instanceof String) {
-                                String personId = (String) result;
-                                realm.executeTransactionAsync(realm -> {
-                                    Person personToDelete = realm.where(Person.class).equalTo("_id", personId).findFirst();
-                                    if (personToDelete != null) {
-                                        personToDelete.deleteFromRealm();
-                                    }
-                                });
+                                deletePerson((String) result);
                             }
                         });
                         RealmResults<Person> people = realm.where(Person.class).findAllAsync();
@@ -91,7 +104,97 @@ public class MainActivity extends AppCompatActivity {
         });
 
         binding.addPersonButton.setOnClickListener(v -> showDialogToAddPerson());
+
+        setupJmDNS();
+        setupNetworkCallback();
     }
+
+    private void setupNetworkCallback() {
+        connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkRequest request = new NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .build();
+
+        connectivityManager.registerNetworkCallback(request, new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                updateOnlineStatus(true);
+            }
+
+            @Override
+            public void onLost(@NonNull Network network) {
+                updateOnlineStatus(false);
+            }
+        });
+    }
+
+    private void updateOnlineStatus(boolean isOnline) {
+        this.isOnline = isOnline;
+        runOnUiThread(() -> {
+            if (isOnline) {
+                Toast.makeText(MainActivity.this, "Internet is available", Toast.LENGTH_SHORT).show();
+                binding.onlineStatus.setText("Online");
+                binding.onlineStatus.setBackgroundColor(getResources().getColor(R.color.online_green));
+            } else {
+                Toast.makeText(MainActivity.this, "Internet is not available", Toast.LENGTH_SHORT).show();
+                binding.onlineStatus.setText("Offline");
+                binding.onlineStatus.setBackgroundColor(getResources().getColor(R.color.offline_red));
+            }
+        });
+    }
+
+    private void setupJmDNS() {
+        new AsyncTask<Void, Void, JmDNS>() {
+            protected JmDNS doInBackground(Void... voids) {
+                try {
+                    // Perform network operation in background
+                    return JmDNS.create(InetAddress.getLocalHost());
+                } catch (IOException e) {
+                    Log.e("JmDNS", "Setup failed", e);
+                    return null;
+                }
+            }
+
+            protected void onPostExecute(JmDNS jmdns) {
+                if (jmdns != null) {
+                    MainActivity.this.jmdns = jmdns;
+                    registerService();
+                } else {
+                    Log.e("JmDNS", "Error setting up JmDNS");
+                }
+            }
+        }.execute();
+    }
+
+    private void registerService() {
+        String type = "_http._tcp.local.";
+        String name = "AndroidApp";
+        int port = 12345;  // The port your service is listening on
+        try {
+            jmdns.registerService(ServiceInfo.create(type, name, port, "path=index.html"));
+
+            jmdns.addServiceListener(type, new ServiceListener() {
+                @Override
+                public void serviceResolved(ServiceEvent ev) {
+                    Log.d("JmDNS", "Service resolved: " + ev.getInfo().getQualifiedName() + " port:" + ev.getInfo().getPort());
+                    applyReceivedData(ev.getInfo().getNiceTextString());
+                }
+
+                @Override
+                public void serviceAdded(ServiceEvent event) {
+                    jmdns.requestServiceInfo(event.getType(), event.getName(), 1);
+                }
+
+                @Override
+                public void serviceRemoved(ServiceEvent ev) {
+                    Log.d("JmDNS", "Service removed: " + ev.getName());
+                }
+            });
+        } catch (IOException e) {
+            Log.e("JmDNS", "Error registering service", e);
+        }
+    }
+
 
     private void showDialogToAddPerson() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -110,19 +213,11 @@ public class MainActivity extends AppCompatActivity {
             int age;
             try {
                 age = Integer.parseInt(ageStr);
+                addOrUpdatePerson(name, age); // Using the new method
             } catch (NumberFormatException e) {
                 Toast.makeText(MainActivity.this, "Invalid age", Toast.LENGTH_SHORT).show();
                 return;
             }
-
-            realm.executeTransactionAsync(realm -> {
-                        Person person = new Person();
-                        person.set_id(String.valueOf(System.currentTimeMillis()));
-                        person.setName(name);
-                        person.setAge(age);
-                        realm.copyToRealmOrUpdate(person);
-                    }, () -> Log.d("MainActivity", "Person added successfully!"),
-                    error -> Log.e("MainActivity", "Error adding person: " + error.getMessage()));
         });
 
         builder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
@@ -131,11 +226,79 @@ public class MainActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    private void addOrUpdatePerson(String name, int age) {
+        realm.executeTransactionAsync(realm -> {
+                    Person person = new Person();
+                    person.set_id(String.valueOf(System.currentTimeMillis()));
+                    person.setName(name);
+                    person.setAge(age);
+                    realm.copyToRealmOrUpdate(person);
+                    if (!isOnline) {
+                        sendUpdateViaJmDNS(new Gson().toJson(person)); // Send update if offline
+                    }
+                }, () -> Log.d("MainActivity", "Person added successfully!"),
+                error -> Log.e("MainActivity", "Error adding person: " + error.getMessage()));
+    }
+
+    private void deletePerson(String personId) {
+        realm.executeTransactionAsync(realm -> {
+            Person personToDelete = realm.where(Person.class).equalTo("_id", personId).findFirst();
+            if (personToDelete != null) {
+                HashMap<String, String> message = new HashMap<>();
+                message.put("action", "delete");
+                message.put("_id", personToDelete.get_id());
+                String jsonMessage = new Gson().toJson(message);
+
+                personToDelete.deleteFromRealm();
+
+                if (!isOnline) {
+                    sendUpdateViaJmDNS(jsonMessage); // Send deletion message if offline
+                }
+            }
+        });
+    }
+
+    private void sendUpdateViaJmDNS(String jsonMessage) {
+        try {
+            ServiceInfo serviceInfo = ServiceInfo.create("_http._tcp.local.", APP_NAME, 12345, jsonMessage);
+            jmdns.registerService(serviceInfo);
+            Log.d("JmDNS", "Sent update via JmDNS: " + jsonMessage);
+        } catch (Exception e) {
+            Log.e("JmDNS", "Error sending update via JmDNS", e);
+        }
+    }
+
+    private void applyReceivedData(String json) {
+        Gson gson = new Gson();
+        HashMap<String, String> data = gson.fromJson(json, HashMap.class);
+        if (data.containsKey("action") && "delete".equals(data.get("action"))) {
+            realm.executeTransactionAsync(r -> {
+                Person personToDelete = r.where(Person.class).equalTo("_id", data.get("_id")).findFirst();
+                if (personToDelete != null) {
+                    personToDelete.deleteFromRealm();
+                }
+            });
+        } else {
+            Person person = gson.fromJson(json, Person.class);
+            realm.executeTransactionAsync(r -> r.insertOrUpdate(person));
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         if (realm != null) {
             realm.close();
+        }
+        if (jmdns != null) {
+            try {
+                jmdns.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (connectivityManager != null) {
+            connectivityManager.unregisterNetworkCallback(new ConnectivityManager.NetworkCallback());
         }
     }
 }
